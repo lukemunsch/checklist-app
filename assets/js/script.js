def69df0b9f31298148f7ef4a49a462125d47d0a
Luke Munsch <lpmunsch@gmail.com>
2023-04-11 21:51:31 +0100

    Update contents of files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
 // Defining my custom variables
-const set1 = Array.from(document.getElementsByClassName('taskSet1'));
-const set2 = Array.from(document.getElementsByClassName('taskSet2'));
-const set3 = Array.from(document.getElementsByClassName('taskSet3'));
-const set4 = Array.from(document.getElementsByClassName('taskSet4'));
+const tasks1 = Array.from(document.getElementsByClassName('taskText'));
 
 let set1CompCount = 0;
-let set2CompCount = 0;
-let set3CompCount = 0;
-let set4CompCount = 0;
+
 
 let taskRequired = true;
 
 function startTasks(){
     set1CompCount = 0;
-    
+
 }
 
 startTasks();
