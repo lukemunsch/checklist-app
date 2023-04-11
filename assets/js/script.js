f11e5fd8457e97035730223887d2063b75ae5526
Luke Munsch <lpmunsch@gmail.com>
2023-04-11 21:24:45 +0100

    Update files for naming and id designations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 // Defining my custom variables
-const tasks1 = Array.from(document.getElementsByClassName('taskSet1'));
-const tasks2 = Array.from(document.getElementsByClassName('taskSet2'));
-const tasks3 = Array.from(document.getElementsByClassName('taskSet3'));
-const tasks4 = Array.from(document.getElementsByClassName('taskSet4'));
+const set1 = Array.from(document.getElementsByClassName('taskSet1'));
+const set2 = Array.from(document.getElementsByClassName('taskSet2'));
+const set3 = Array.from(document.getElementsByClassName('taskSet3'));
+const set4 = Array.from(document.getElementsByClassName('taskSet4'));
 
 let set1CompCount = 0;
 let set2CompCount = 0;
 let set3CompCount = 0;
 let set4CompCount = 0;
 
-let taskRequired = {};
+let taskRequired = true;
 
 function startTasks(){
-
+    set1CompCount = 0;
+    
 }
 
 startTasks();
