42ff6a59beaa16acb3be7b0556f29ccc0eee79a3
Luke Munsch <lpmunsch@gmail.com>
2023-04-11 20:39:40 +0100

    Add new tasks list and styles for checklist page

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Defining my custom variables
+const task1 = Array.from(document.getElementsByClassName('tasksSet1'));
+const task2 = Array.from(document.getElementsByClassName('tasksSet2'));
+const task3 = Array.from(document.getElementsByClassName('tasksSet3'));
+const task4 = Array.from(document.getElementsByClassName('tasksSet4'));
+
+
+function startTasks(){
+
+}
+
+startTasks();
---
+++ assets/js/tasks.js
@@ -0,0 +1,14 @@
+var firstSet = [
+    {
+        task: "Scrub the hyperdrive"
+    },
+    {
+        task: "Train the salamanders"
+    },
+    {
+        task: "Teach robin how to fire a bow and arrow"
+    },
+    {
+        task: "Clean Deadpool's mask... again"
+    },
+];
