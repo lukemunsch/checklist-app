6fa579173205f43c829c1d3446fbb05ba5e7ca0a
Luke Munsch <lpmunsch@gmail.com>
2023-04-12 20:05:11 +0100

    Update files structure for app

--- assets/js/taskSet1.js
+++
@@ -1,9 +0,0 @@
-var firstSet = [
-    {
-        taskSetTitle: "Preparations",
-        task1: "Scrub the hyperdrive to clean away the Tatooine sand",
-        task2: "Train Salamander not to set fire to Lucy",
-        task3: "Teach Robin how to fire a Bow and Arrow",
-        task4: "Clean Deadpool's... Mask, please let it be the mask!",
-    },
-];
