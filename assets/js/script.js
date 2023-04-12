bf1123272f9f04124486084747c6f5992bc5c0af
Luke Munsch <lpmunsch@gmail.com>
2023-04-12 20:24:23 +0100

    Update reload button and checklist text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// Defining my custom variables
-const tasks1 = Array.from(document.getElementsByClassName('taskText'));
-
-let set1CompCount = 0;
-
-
-let taskRequired = true;
-
-function startTasks(){
-    set1CompCount = 0;
-
+function reload() {
+    location.reload();
 }
-
-startTasks();
