1ee74949a54d1b14efa214aa7f52f50c7e55ad81
Luke Munsch <lpmunsch@gmail.com>
2023-04-19 20:11:16 +0100

    Add new content to script.js for checkbox count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,44 @@
 function reload() {
     location.reload();
 }
+
+let checkbox1 = $("input[type=checkbox][name=set1]")
+let comp1 = [];
+let checkbox2 = $("input[type=checkbox][name=set2]")
+let comp2 = [];
+let checkbox3 = $("input[type=checkbox][name=set3]")
+let comp3 = [];
+let checkboxes = $("input[type=checkbox]");
+let enablePrint = [];
+
+checkbox1.change(function() {
+    comp1 = checkbox1
+        .filter(":checked")
+        .length;
+
+    $("")
+})
+
+checkbox2.change(function() {
+    comp2 = checkbox2
+        .filter(":checked")
+        .length;
+
+    console.log(comp2)
+})
+
+checkbox3.change(function() {
+    comp3 = checkbox3
+        .filter(":checked")
+        .length;
+
+    $("")
+})
+
+checkboxes.change(function() {
+    enablePrint = checkboxes
+        .filter(":checked")
+        .length;
+
+    $("")
+})
